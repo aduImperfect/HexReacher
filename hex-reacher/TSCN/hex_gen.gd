9d8d2aa0c8dbbb118d3864d0d72882adafff2ffb
Aditya Subramanian <adu101992@gmail.com>
2026-06-15 00:52:19 +0530

    blocked by walls.. and path to goall and game end ready

--- hex-reacher/TSCN/hex_gen.gd
+++ hex-reacher/TSCN/hex_gen.gd
@@ -13,23 +13,50 @@ const ENEMY3_SCENE = preload("res://TSCN/enemy3.tscn")
 @export var hexCalculationsNode : Node2D
 
 @export var groundArr : Array[Node2D] = []
+@export var groundXArr : Array[int] = []
+@export var groundYArr : Array[int] = []
+
 @export var wallArr : Array[Node2D] = []
+@export var wallXArr : Array[int] = []
+@export var wallYArr : Array[int] = []
+
 @export var coinArr : Array[Node2D] = []
+@export var coinXArr : Array[int] = []
+@export var coinYArr : Array[int] = []
+
 @export var enemy1Arr : Array[Node2D] = []
+@export var enemy1XArr : Array[int] = []
+@export var enemy1YArr : Array[int] = []
+
 @export var enemy2Arr : Array[Node2D] = []
+@export var enemy2XArr : Array[int] = []
+@export var enemy2YArr : Array[int] = []
+
 @export var enemy3Arr : Array[Node2D] = []
+@export var enemy3XArr : Array[int] = []
+@export var enemy3YArr : Array[int] = []
 
 @export var groundNum : int = 0
 @export var groundDiv : int = 0
-@export var wallNum : int = 0
+
+@export var wallMaxNum : int = 0
+@export var wallCount : int = 0
 @export var wallDiv : int = 0
-@export var coinNum : int = 0
+
+@export var coinMaxNum : int = 0
+@export var coinCount : int = 0
 @export var coinDiv : int = 0
-@export var enemy1Num : int = 0
+
+@export var enemy1MaxNum : int = 0
+@export var enemy1Count : int = 0
 @export var enemy1Div : int = 0
-@export var enemy2Num : int = 0
+
+@export var enemy2MaxNum : int = 0
+@export var enemy2Count : int = 0
 @export var enemy2Div : int = 0
-@export var enemy3Num : int = 0
+
+@export var enemy3MaxNum : int = 0
+@export var enemy3Count : int = 0
 @export var enemy3Div : int = 0
 
 @export var startNode : Node2D
@@ -60,6 +87,26 @@ func _ready() -> void:
 	groundNum = 361
 	groundDiv = 19
 
+	wallMaxNum = 361
+	wallCount = 0
+	wallDiv = 19
+
+	coinMaxNum = 361
+	coinCount = 0
+	coinDiv = 19
+
+	enemy1MaxNum = 361
+	enemy1Count = 0
+	enemy1Div = 19
+
+	enemy2MaxNum = 361
+	enemy2Count = 0
+	enemy2Div = 19
+
+	enemy3MaxNum = 361
+	enemy3Count = 0
+	enemy3Div = 19
+
 	spawnedEverything = false
 
 
@@ -71,6 +118,7 @@ func _process(_delta: float) -> void:
 		_spawn_start()
 		_spawn_goal()
 		_spawn_player()
+		_spawn_walls()
 	spawnedEverything = true
 
 func _spawn_ground() -> void:
@@ -86,6 +134,9 @@ func _spawn_ground() -> void:
 		ground_instance.global_position.y = hexCalculationsNode._hex_pos_y(_xBegin, _yBegin)
 		add_child(ground_instance)
 		groundArr.append(ground_instance)
+		groundXArr.append(_xBegin)
+		groundYArr.append(_yBegin)
+		#_increment(_xBegin, _yBegin, groundNum, groundDiv)
 		_xBegin += 1
 		if (_xBegin > int(groundNum / float(groundDiv))):
 			_xBegin = 1
@@ -111,3 +162,55 @@ func _spawn_player() -> void:
 	player_instance.global_position.y = hexCalculationsNode._hex_pos_y(playerPosX, playerPosY)
 	add_child(player_instance)
 	playerNode = player_instance
+
+func _spawn_walls() -> void:
+	var wall_instance
+	var _xBegin : int = 1
+	var _yBegin : int = 1
+
+	for k in wallMaxNum:
+		if _check_if_occupied(_xBegin, _yBegin):
+			_xBegin += 1
+			if (_xBegin > int(wallMaxNum / float(wallDiv))):
+				_xBegin = 1
+				_yBegin += 1
+			continue
+
+		var makeWallRand : float = randf_range(0.0, 100.0)
+
+		if makeWallRand >= 30.0:
+			_xBegin += 1
+			if (_xBegin > int(wallMaxNum / float(wallDiv))):
+				_xBegin = 1
+				_yBegin += 1
+			continue
+
+		wall_instance = WALL_SCENE.instantiate()
+		#multiplied decrement by each increase of the yVal.
+		#on being even -  make sure to take the lower valued decrement to offset the hex
+		wall_instance.global_position.x = hexCalculationsNode._hex_pos_x(_xBegin, _yBegin)
+		wall_instance.global_position.y = hexCalculationsNode._hex_pos_y(_xBegin, _yBegin)
+		add_child(wall_instance)
+		wallArr.append(wall_instance)
+		wallXArr.append(_xBegin)
+		wallYArr.append(_yBegin)
+		wallCount += 1
+
+		_xBegin += 1
+		if (_xBegin > int(wallMaxNum / float(wallDiv))):
+			_xBegin = 1
+			_yBegin += 1
+
+func _check_if_occupied(_newX : int, _newY : int) -> bool:
+		if _check_if_occupied_internal(startPosX, startPosY, _newX, _newY):
+			return true
+		if _check_if_occupied_internal(goalPosX, goalPosY, _newX, _newY):
+			return true
+		if _check_if_occupied_internal(playerPosX, playerPosY, _newX, _newY):
+			return true
+		return false
+
+func _check_if_occupied_internal(_x : int, _y : int, _newX : int, _newY : int) -> bool:
+	if (_x == _newX) && (_y == _newY):
+		return true
+	return false
